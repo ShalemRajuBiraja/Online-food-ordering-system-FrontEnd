0c03b7a42fe03127781683ed6ec9c4148c135476
Shalem Raju <shalemrajubiraja7@gmail.com>
2026-07-30 20:46:57 +0530

    Modified the config.js file

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/food-ordering-website-frontend/",
+  base: "/food-ordering-website/",
 });
